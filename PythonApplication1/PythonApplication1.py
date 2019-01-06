62f1a1693133df8ac6840a3fbac0309085041f13
短田慎一 <elmas@tandenshinichinoMacBook-Pro.local>
2019-01-06 15:33:14 +0900

    チェック処理を修正

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,5 +3,5 @@ import requests
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 
 type(res)
-res.status_code == requests.codes.ok
-print(res.text[:250])
+if res.status_code == 200 :
+    print(res.text[:250])
